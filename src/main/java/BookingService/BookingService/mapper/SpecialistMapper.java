a4bcfde2281c7dd5ad6092a5861d970d068a61f0
Truong <truongltse180010@fpt.edu.vn>
2025-03-07 11:27:56 +0700

    update scpecialist

---
+++ src/main/java/BookingService/BookingService/mapper/SpecialistMapper.java
@@ -0,0 +1,23 @@
+package BookingService.BookingService.mapper;
+
+import BookingService.BookingService.dto.response.SpecialistResponse;
+import BookingService.BookingService.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface SpecialistMapper {
+    SpecialistMapper INSTANCE = Mappers.getMapper(SpecialistMapper.class);
+
+    @Mapping(source = "userId", target = "userId")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "phone", target = "phone")
+    @Mapping(source = "address", target = "address")
+    @Mapping(source = "role", target = "role")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "createdAt", target = "createdAt")
+    @Mapping(source = "updatedAt", target = "updatedAt")
+    SpecialistResponse toSpecialistResponse(User user);
+}
