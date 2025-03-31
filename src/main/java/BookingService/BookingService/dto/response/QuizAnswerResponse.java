73bc5def6f91ef98d048b4ac00ef6c5bbc364eee
Hoang Nam <namhoang0505pro@gmail.com>
2025-03-31 15:15:34 +0700

    quiz update

--- src/main/java/BookingService/BookingService/dto/response/QuizAnswerResponse.java
+++ src/main/java/BookingService/BookingService/dto/response/QuizAnswerResponse.java
@@ -1,5 +1,6 @@
 package BookingService.BookingService.dto.response;
 
+import BookingService.BookingService.enums.SkinType;
 import lombok.Data;
 
 @Data
@@ -7,4 +8,5 @@ public class QuizAnswerResponse {
     private Long id;
     private String answerText;
     private int score;
+    private SkinType skinType; // Thêm trường skinType
 }
